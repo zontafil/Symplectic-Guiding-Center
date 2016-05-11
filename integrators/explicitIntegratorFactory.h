0124265c691a8c4f101f5bc7aa9aa138121ad6ba
Filippo Zonta <kknull0@gmail.com>
2016-05-11 17:55:06 +0200

    Add missing explicit integrator factory class

---
+++ integrators/explicitIntegratorFactory.h
@@ -0,0 +1,26 @@
+#ifndef EXPLICITINTEGRATORFACTORY_H
+#define EXPLICITINTEGRATORFACTORY_H
+
+#include <stdexcept>
+#include "explicitIntegrator.h"
+#include "explicit/symplecticExplicit1.h"
+#include "explicit/symplecticExplicit2.h"
+#include "explicit/symplecticExplicit3.h"
+#include "explicit/symplecticExplicit4.h"
+#include "explicit/RK4.h"
+
+using namespace std;
+
+namespace Integrators{
+	template <int DIM> ExplicitIntegrator<DIM> *explicitIntegratorFactory(std::string const& integratorName, Config::Config* config){
+		if (integratorName=="SymplecticExplicit1") return new SymplecticExplicit1<DIM>(config);
+		else if (integratorName=="SymplecticExplicit2") return new SymplecticExplicit2<DIM>(config);
+		else if (integratorName=="SymplecticExplicit3") return new SymplecticExplicit3<DIM>(config);
+		else if (integratorName=="SymplecticExplicit4") return new SymplecticExplicit4<DIM>(config);
+		else if (integratorName=="RK4") return new RK4<DIM>(config);
+		else throw invalid_argument("Invalid explicit integrator "+ integratorName);
+	}
+
+}
+
+#endif
